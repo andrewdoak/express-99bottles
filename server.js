e3edf31c269d465b713138e9b6cf627b92186ad8
andrew <thedoak@gmail.com>
2023-08-31 16:40:45 -0500

    Core functionality working. Debug bonus in progress.

--- server.js
+++ server.js
@@ -1,106 +1,65 @@
 // Load express
 const express = require('express');
-
-// Create our express app
+// Create express app
 const app = express();
+// Define port as process.env or 3000
+const PORT = process.env.PORT || 3000;
+
+
 // Define a "root" route directly on app
-// Tomorrow, we'll use best practice routing
-// Mount routes
+// MOUNT 'ROOT' ROUTE
 app.get('/', function(req, res) {
     res
     .send(`
-    <h1>99 Bottles of Beer on the Wall.</h1>
-    <a href="#">Pass it around</a>
+    <h2>99 bottles of beer on the wall...</h2>
+    <a href="/98">Take one down...</a>
     `);
   });
 
-/* 
-ASHLEY: HW START
-<h1>${currentBottles} Bottles of Beer on the Wall.</h1>
-<a href=“/take-one-down”>${currentBottles === 0? “No :”Take}</a>
-*/
+// 99 BOTTLES
+app.get('/:number_of_bottles', (req, res) => {
+    let nextPage = parseInt(req.params.number_of_bottles) - 1
+    if (parseInt(req.params.number_of_bottles) > 1) {
+        res.send(`<h2>${req.params.number_of_bottles} bottles of beer on the wall.</h2>
+        <a href='${nextPage}'>Take one down, pass it around</a>`)
+    } else if (parseInt(req.params.number_of_bottles) === 1) {
+        res.send(`<h2>${req.params.number_of_bottles} just ONE MORE bottle of beer on the wall.</h2>
+        <a href='${nextPage}'>Take one down, pass it around</a>`)
+    } else {
+        res.send(`
+        <h2>We're out of bottles!</h2>
+        <a href="/">Better make a store run. Get some veggie chips, too!</a>`);
+    }
+})
+
+// BONUS 99 little bugs
+app.get('/debug', (req, res) => {
+    res.send (`<h2>99 Little bus in the code</h2><h2>99 Little bugs</h2>
+    <a href="/debug/123>Take one down patch it around</a>
+    `);
+});
+
+// BONUS 99 Little Bugs Continued
+app.get('/bug/:num', (req,res) => {
+    let rdm = Math.floor(Math.random() * 50) + 1;
+    let nextBug = parseInt(req.params.num) + rdm;
+    res.send(`
+        <h2>${req.params.num} Little bugs in the code</h2><h3>${req.params.num} Little bugs</h3>
+        <a href ='/bug/${nextBug}'>Take one down patch it around</a>
+    `);
+});
 
 // Tell the app to listen on port 3000
 // for HTTP requests from clients
 app.listen(3000, function () {
-  console.log('Listening on port 3000');
+  console.log(`Listening on port ${PORT}`);
 });
 
 /* 
 HOMEWORK URL
 https://ps-rtt-sei.herokuapp.com/15-week/mod-3/week-11/day-2/hw/
 
-REMEMBER, RIGHT NOW, IF YOU MAKE SOME CHANGES, 
-YOU HAVE TO STOP AND START YOUR SERVER
-BUT NODEMON SAVES THE DAY
-WATCHES YOUR FILES FOR CHANGES, THEN DOES THE SERVER RESTART WORK
-RUN NODEMON => SAME LIKE RUNNING NODE FILES 'nodemon your-file.js'
-
-
 NOTES/CODE COMMENTS
 ====================
 
-OUTLINE OF EXPRESS SERVER
-// 1. REQUIRE MODULES
-// IT'S A FUNCTION
-// DIFFERENT PACKAGES REQUIRE DIFFERENT DATATYPES
-const express = require('express');
-
-
-// 2. CREATE APP
-// Express method
-const app = express();
-
-// IF YOU WANT TO SEE WHAT'S GOING ON
-    // console.log(app)
-
-
-// 3. CONFIGURE APP 
-    // (app.set)
-
-
-// 4. MOUNT MIDDLEWARE 
-    // (app.use)
-
-
-// 5. MOUNT ROUTES
-app.get('/', function(req, res) {
-  	res.send('<h1>Hello World!</h1>');
-});
-    // Can check out Headers, Preview, Response in Network Tab (Inspect)
-
-    // Specifies what happens when a user makes a request to our server.
-    // Base URL
-    // PATH, ALWAYS A STRING, ALWAYS A FORWARD SLASH
-    // ALWAYS HAS A REQUEST AND RESPONSE (req, res) 
-    // .send Method SENDS A RESPONSE (HTML in this case). 
-    // SIMILAR TO return. CAN'T HAPPEN 2x
-    // RESPONSE SHOULD ALWAYS BE LAST
-
-    // In the res.send, it only runs when you get a response.
-        // Verb AND Path have to match.
-    // This is where we write logic for anything we want to return.
-    // Could be login or other stuff than HTML.
-
-    // MOST USED METHODS
-        // res.render()- Render a view template and send the resulting HTML to the browser.
-        // res.redirect()- Tell the browser to issue another GETrequest.
-        // res.json()- Send a JSON response (used when we build an API).
-        // you can also chain some methods on, like status()
-    
-    // YOU CAN DEFINE MORE THAN ONE ROUTE ON THE SAME PATH
-        // GET
-        // POST
-        // DELETE
-        // PUT 
-        // but not more than one of each verb 
-
-// 6. LISTEN ON p 3000
-    // NEEDS A CALLBACK FUNCTION, COULD ALSO USE AN ARROW FUNCTION
-    // FUNCTION IS CALLED AFTER WE KNOW THE SERVER'S LISTENING
-    // THAT clg MESSAGE PRINTS ONCE WE KNOW IT'S LISTENING
-
-app.listen(3000, function() {
- console.log('Listening on port 3000');
-});
 */
